f6537debe84638ac464dc93bc798e405dda43254
Egor <semenenkoe964@gmail.com>
2025-10-03 09:21:59 +0300

    Create CaesarCipher.py

---
+++ CaesarCipher.py
@@ -0,0 +1,25 @@
+offset=int(input())
+cipher=input()
+txt=input()
+newTxt=""
+alphabet=0
+startAlpha=1040
+endAlpha=1103
+if 65<=ord(txt[0])<=122:
+    alphabet=1
+if alphabet:
+    startAlpha=65
+    endAlpha=122
+if cipher=="1":
+    for i in txt:
+        if ord(i)+offset<=endAlpha:
+            newTxt+=chr(ord(i)+offset)
+        else:
+            newTxt += chr(ord(i) + offset-endAlpha+startAlpha)
+else:
+    for i in txt:
+        if ord(i)-offset>=startAlpha:
+            newTxt+=chr(ord(i)-offset)
+        else:
+            newTxt += chr(endAlpha-(startAlpha-(ord(i) + offset)))
+print(newTxt)
