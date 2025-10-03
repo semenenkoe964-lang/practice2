7ed023830f0c3f08a8778c06bb8d35fad78ce4c5
Egor <semenenkoe964@gmail.com>
2025-10-03 15:26:27 +0300

    Update CaesarCipher.py

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -2,14 +2,11 @@ offset=int(input("Введите сдвиг: "))
 cipher=input("Шифр 1 , Дешифр 0: ")
 txt=input("Текст :")
 newTxt=""
-alphabet=0
 startAlpha=1040
 endAlpha=1103
 if 65<=ord(txt[0])<=122:
-    alphabet=1
-if alphabet:
-    startAlpha=65
-    endAlpha=122
+    startAlpha = 65
+    endAlpha = 122
 if cipher=="1":
     for i in txt:
         if ord(i)+offset<=endAlpha:
